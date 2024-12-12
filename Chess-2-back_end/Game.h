c4a3dd825f23871afac264bab31b49a71f72e59b
borisov1evgeniy <g55rus@mail.ru>
2024-12-12 20:44:03 +0300

    Update Game.h

--- Chess-2-back_end/Game.h
+++ Chess-2-back_end/Game.h
@@ -19,6 +19,5 @@ public:
     Game();
     std::vector<std::pair<int, int>> future_go(std::pair<int, int> coordinates);
     void move(std::pair<int, int> coordinates_new);
-    void print();
 };
 #endif
